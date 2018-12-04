04e9cc80787bc374f058720943132ba3bcf0c373
Nikita Rodin <n1k1tung4tc@gmail.com>
2018-12-04 14:48:41 +0300

    tag id

--- Source/WSTag.swift
+++ Source/WSTag.swift
@@ -10,18 +10,25 @@ import Foundation
 
 public struct WSTag: Hashable {
 
+    public let id: String
     public let text: String
 
+    public init(_ id: String, _ text: String) {
+        self.id = id
+        self.text = text
+    }
+    
     public init(_ text: String) {
+        self.id = text
         self.text = text
     }
 
     public var hashValue: Int {
-        return self.text.hashValue
+        return self.id.hashValue
     }
 
     public func equals(_ other: WSTag) -> Bool {
-        return self.text == other.text
+        return self.id == other.id
     }
 
 }
